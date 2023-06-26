b7d5c2500ede21f83c6cccd9ea0e072874590437
AYnonyme <110267060+AYnonyme971@users.noreply.github.com>
2023-06-25 20:08:30 -0400

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const Password = require('./index');
-const password = new Password(12, true, true, true);
-let pass = password.random();
-console.log(pass);
-pass = password.random();
-console.log(pass)
